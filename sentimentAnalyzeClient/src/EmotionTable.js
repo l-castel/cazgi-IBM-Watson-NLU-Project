68256ef05fc64d6834701394da8a39dd74517dbd
Lola Castello <71978109+l-castel@users.noreply.github.com>
2021-06-17 17:05:50 +0200

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -3,13 +3,19 @@ import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
-      return (  
+        let emotionList = Object.entries(this.props.emotions.emotion);
+        let emotionTable = emotionList.map((emotion)=> {
+            return <tr>
+                <td >{emotion[0]} </td>
+                <td > {emotion[1]} </td>
+            </tr>
+        });
+      return (
         <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
           <table className="table table-bordered">
             <tbody>
             {
+                emotionTable
                 //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
             }
             </tbody>
@@ -17,6 +23,6 @@ class EmotionTable extends React.Component {
           </div>
           );
         }
-    
+
 }
 export default EmotionTable;
--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,27 +1,36 @@
 const express = require('express');
+const app = new express();
 const dotenv = require('dotenv');
 dotenv.config();
-const app = new express();
 
-app.use(express.static('client'))
+
+function getParams (srcType, data, analyzeType) {
+    let anParams = {};
+    anParams[srcType] = data;
+    anParams.features = {}
+    anParams.features[analyzeType] = {};
+    return anParams;
+}
 
 
 function getNLUInstance() {
     let api_key = process.env.API_KEY;
     let api_url = process.env.API_URL;
-
     const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
     const { IamAuthenticator } = require('ibm-watson/auth');
 
     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
         version: '2021-06-17',
         authenticator: new IamAuthenticator({
-            apikey = api_key,
+            apikey: api_key,
         }),
-        serviceUrl = api_url,
+        serviceUrl: api_url,
     });
     return naturalLanguageUnderstanding;
 }
+
+app.use(express.static('client'))
+
 const cors_app = require('cors');
 app.use(cors_app());
 
@@ -30,20 +39,43 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
+    getNLUInstance().analyze(getParams('url', req.query.url, 'emotion'))
+        .then(analysisResults => {
+            return res.send(JSON.stringify(analysisResults, null, 2));
+        })
+        .catch(err => {
+            return res.send(err.toString());
+        });
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    getNLUInstance().analyze(getParams('url', req.query.url, 'sentiment',))
+        .then(analysisResults => {
+            return res.send(JSON.stringify(analysisResults, null, 2));
+        })
+        .catch(err => {
+            return res.send(err.toString());
+        });
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    getNLUInstance().analyze(getParams('text',req.query.text, 'emotion', ))
+        .then(analysisResults => {
+          return res.send(JSON.stringify(analysisResults, null, 2));
+        })
+        .catch(err => {
+          return res.send(err.toString());
+        });
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    getNLUInstance().analyze(getParams('text',req.query.text, 'sentiment', ))
+        .then(analysisResults => {
+            return res.send(JSON.stringify(analysisResults, null, 2));
+        })
+        .catch(err => {
+            return res.send(err.toString());
+        });
 });
 
 let server = app.listen(8080, () => {
